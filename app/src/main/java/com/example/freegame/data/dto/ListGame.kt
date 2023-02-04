9d2bddf75a71b7504d72e6d1c7c222b1a863ad1d
Nikita <ropotov.dev@gmail.com>
2023-02-04 19:23:20 +0400

    initial commit

---
+++ app/src/main/java/com/example/freegame/data/dto/ListGame.kt
@@ -0,0 +1,3 @@
+package com.example.freegame.data.dto
+
+data class ListGame (val listGame: List<ListGameItem>?)
---
+++ app/src/main/java/com/example/freegame/data/dto/ListGameItem.kt
@@ -0,0 +1,14 @@
+package com.example.freegame.data.dto
+
+data class ListGameItem(
+    val developer: String?,
+    val game_url: String?,
+    val genre: String?,
+    val id: Int?,
+    val platform: String?,
+    val publisher: String?,
+    val release_date: String?,
+    val short_description: String?,
+    val thumbnail: String?,
+    val title: String?
+)
